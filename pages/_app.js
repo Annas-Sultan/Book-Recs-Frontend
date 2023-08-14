3417e54733f4762cef0478c63b40baef59b8fe8c
Annas Sultan <annas.sultan@gmail.com>
2023-08-14 18:12:52 -0500

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
+import { Analytics } from '@vercel/analytics/react';
 import client from '../apollo-client';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
+      <Analytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
